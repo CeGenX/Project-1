754d43966b0eaa1440d9f300191a3a40c036676b
CeGenX <34990672+CeGenX@users.noreply.github.com>
2017-12-31 16:51:32 -0700

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -24,3 +24,35 @@ void menu()
     readinput();
   }
 }
+void readinput()
+{
+	string v;
+	vector<double> n;
+	int val = 1;
+	int m = 0;
+	getline(cin, string& v);
+	for(int i = 0; v.size() > i; i++)
+	{
+		if( (v.at(i) <= '9' || v.at(i) <= '0'))
+		{
+			if( i >= 1 && v.at(i-1) == '-')
+				{
+					val*=-1;
+				}
+			n.at(m) = (v.at(i))*val;
+			m++:
+		}
+	summation(v,0,m);
+	
+}
+
+
+void summation(vector<double> x,int beg,int end)
+{
+	int sum = 0;
+	for (int i = 0; x.size() > i; i++)
+	{
+		sum += x.at(i);
+	}
+	cout<< sum;
+}
